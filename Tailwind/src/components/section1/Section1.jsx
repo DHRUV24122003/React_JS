59ae605ad3bb17ee9745562cc5476cc0d1c82f21
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-07-14 21:34:01 +0530

    //

---
+++ Tailwind/src/components/section1/Section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className ="h-screen w-full bg-green-600">section1
+      
+    </div>
+  ) 
+  
+}
+
+export default Section1
