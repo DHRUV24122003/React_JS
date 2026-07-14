964172e4b14712743cce6d590d67b73b7d9dd8cf
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-07-14 16:44:28 +0530

    commit changes

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,37 +1,14 @@
 import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
   return (
-    <div className ="parent">
-      <div className = "Card">
-        <h1>Dhruv Khanna</h1>
-        <img src="https://plus.unsplash.com/premium_photo-1781710760389-dbc993cf7e0e?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8" alt="Profile" />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
-
-         <div className = "Card">
-        <h1>Dhruv Khanna</h1>
-        <img src="https://plus.unsplash.com/premium_photo-1781710760389-dbc993cf7e0e?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8" alt="Profile" />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
-
-
-         <div className = "Card">
-        <h1>Dhruv Khanna</h1>
-        <img src="https://plus.unsplash.com/premium_photo-1781710760389-dbc993cf7e0e?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8" alt="Profile" />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        <button>View Profile</button>
-      </div>
+    <div className='parent'>
+      <Card user='Aman Singh' age={18} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Sarthak' age={28} img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Aditya' age={24} img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
     </div>
-
-
-
-
-    
-
   )
-} 
+}
 
 export default App
