eaffde9e3637d56a1024049de1e32d459546a922
dhruvkhanna <dhruvkhanna2163@gmail.com>
2026-07-15 20:41:36 +0530

    tailwind project

--- Tailwind/src/components/section1/Section1.jsx
+++ Tailwind/src/components/section1/Section1.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Navbar from './Navbar'
+import Page1Content from './Page1Content'
 
-const Section1 = () => {
+const Section1 = (props) => {
   return (
-    <div className ="h-screen w-full bg-green-600">section1
-      
+    <div className='h-screen w-full '>
+        <Navbar />
+        <Page1Content users={props.users} />
     </div>
-  ) 
-  
+  )
 }
 
 export default Section1
